19f91ddf30a4ebc15943b22ae7ecbc67bb65d43e
dzyzas <pharis@o2.pl>
2015-12-06 12:03:40 +0100

    first commit

---
+++ src/main/java/wwsis/nexus/App.java
@@ -0,0 +1,19 @@
+package wwsis.nexus;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	private static final Logger Logger = LoggerFactory.getLogger(App.class);
+	
+    public static void main( String[] args )
+    {
+    	Logger.info("app start");
+        System.out.println( "Hello World!" );
+    }
+}
